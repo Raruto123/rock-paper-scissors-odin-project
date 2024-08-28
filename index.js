6c56c4658a5b659e423888adfe034b96a67dbb46
mohamedsane <sanem9206@gmail.com>
2024-08-28 23:21:04 +0000

    Update the game
    Logic done now a clean user interface to make him more appealing

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+let humanScoreDisplay = document.getElementById("human-score");
+let computerScoreDisplay = document.getElementById("computer-score");
+let winner = document.getElementById("winner-annoucement");
+let moves = document.querySelector(".moves");
+let rockButton = document.querySelector(".rock");
+let paperButton = document.querySelector(".paper");
+let scissorsButton = document.querySelector(".scissors");
+let humanChoiceDisplay = document.querySelector(".humanChoice");
+let computerChoiceDisplay = document.querySelector(".computerChoice");
+
+
+
+
+
 let humanScore = 0;
 let computerScore = 0;
 
@@ -6,130 +20,132 @@ let computerScore = 0;
 function getComputerChoice() {
     let numberReturned = Math.floor(Math.random() * 100);
     let choice = "";
+    computerChoiceDisplay.textContent = "";
     if (numberReturned <= 33) {
         choice = "Rock";
+        computerChoiceDisplay.textContent = "Rock";
     } else if (numberReturned > 33 && numberReturned <= 66) {
         choice = "Paper";
+        computerChoiceDisplay.textContent = "Paper";
+
     } else if (numberReturned > 66 && numberReturned <= 99) {
         choice = "Scissors";
+        computerChoiceDisplay.textContent = "Scissors";
+
     } else {
         choice  = null;
     }
-    console.log(`COMPUTER CHOICE THIS ROUND : ${choice}`);
 
-    return choice;
-}
-
-
-function getHumanChoice() {
-    let options = prompt("Type your choice : Rock - Paper - Scissors ?");
-    let inventory = ["Rock", "Paper", "Scissors"];
-
-    if (options !== null && options.toLocaleLowerCase().includes(inventory[0].toLocaleLowerCase())) {
-        options = inventory[0];
-    } else if (options !== null && options.toLocaleLowerCase().includes(inventory[1].toLocaleLowerCase())) {
-        options = inventory[1];
-    } else if (options !== null && options.toLocaleLowerCase().includes(inventory[2].toLocaleLowerCase())) {
-        options = inventory[2];
-    } else if (options === null){
-        options = "no value chosen";
-    } else {
-        options = "no valid value";
-    }
-    console.log(`PLAYER CHOICE THIS ROUND : ${options}`);
-
-
-    return options;
+    return computerChoiceDisplay.textContent;
 };
 
+// function getHumanChoice(options) {
+//     let inventory = ["Rock", "Paper", "Scissors"];
+
+
+//     if (options !== null && options.toLowerCase().includes(inventory[0].toLowerCase())) {
+//         options = inventory[0];
+//     } else if (options !== null && options.toLowerCase().includes(inventory[1].toLowerCase())) {
+//         options = inventory[1];
+//     } else if (options !== null && options.toLowerCase().includes(inventory[2].toLowerCase())) {
+//         options = inventory[2];
+//     } else if (options === null){
+//         options = "no value chosen";
+//     } else {
+//         options = "no valid value";
+//     }
+//     console.log(`PLAYER CHOICE THIS ROUND : ${options}`);
+
+
+//     console.log("🚀 ~ getHumanChoice ~ choices:", options);
+
+//     return options;
+// };
 
 function playRound(humanChoice, computerChoice) {
 
     if (humanChoice !== null && humanChoice.includes("Rock")) {
         if (computerChoice.includes("Rock")) {
-            console.log(`Equal! ${humanChoice} has no efficacity on ${computerChoice}`);
+            moves.textContent = `Equal! ${humanChoice} has no efficacity on ${computerChoice}`;
         } else if (computerChoice.includes("Paper")) {
-            console.log(`You lose! ${humanChoice} is beaten by ${computerChoice}`);
-            console.log(`Computer Score = ${computerScore += 1}`);
+            moves.textContent = `You lose! ${humanChoice} is beaten by ${computerChoice}`;
+            computerScore += 1;
+            computerScoreDisplay.textContent = computerScore.toString();
         } else {
-            console.log(`Human Score = ${humanScore += 1}`);
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+            humanScore += 1;
+            humanScoreDisplay.textContent = humanScore.toString();
+            moves.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
         }
     } else if (humanChoice !== null && humanChoice.includes("Paper")) {
         if (computerChoice.includes("Rock")) {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-            console.log(`Human Score = ${humanScore += 1}`);
+            moves.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
+            humanScore += 1;
+            humanScoreDisplay.textContent = humanScore.toString();
         } else if (computerChoice.includes("Paper")) {
-            console.log(`Equal! ${humanChoice} has no efficatity on ${computerChoice}`);
+            moves.textContent = `Equal! ${humanChoice} has no efficatity on ${computerChoice}`;
         } else {
-            console.log(`You lose! ${humanChoice} is beaten by ${computerChoice}`);
-            console.log(`Computer Score = ${computerScore += 1}`);
+            moves.textContent = `You lose! ${humanChoice} is beaten by ${computerChoice}`;
+            computerScore += 1;
+            computerScoreDisplay.textContent = computerScore.toString();
         }
     } else if (humanChoice !== null && humanChoice.includes("Scissors")) {
         if (computerChoice.includes("Rock")) {
-            console.log(`You lose! ${humanChoice} is beaten by ${computerChoice}`);
-            console.log(`Computer Score = ${computerScore += 1}`);
+            moves.textContent = `You lose! ${humanChoice} is beaten by ${computerChoice}`;
+            computerScore += 1;
+            computerScoreDisplay.textContent = computerScore.toString();
         } else if (computerChoice.includes("Paper")) {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-            console.log(`Human Score = ${humanScore += 1}`);
+            moves.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
+            humanScore += 1;
+            humanScoreDisplay.textContent = humanScore.toString();
         } else {
-            console.log(`Equal! ${humanChoice} has no efficacity on ${computerChoice}`);
+            moves.textContent = `Equal! ${humanChoice} has no efficacity on ${computerChoice}`;
         }
     } else if (humanChoice === null) {
-        console.log(`You need to select an option to play!`);
+        moves.textContent = `You need to select an option to play!`;
     } else {
-        console.log(`The Player choice is not valid! please type a valid option`);
+        moves.textContent = `The Player choice is not valid! please type a valid option`;
     }
-
-
-
 };
-// const humanSelection = getHumanChoice();
-// const computerSelection = getComputerChoice();
 
-// playRound(humanSelection, computerSelection)
+
 
 function playGame() {
 
-for (let i = 1; i <= 5; i++) {
-    if (i === 1) {
-        console.log("ROUND 1 START!");
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-
-    if (i === 2) {
-        console.log("ROUND 2 START!");
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-
-    if (i === 3) {
-        console.log("ROUND 3 START!");
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-
-    if (i === 4) {
-        console.log("ROUND 4 START!");
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-
-    if (i === 5) {
-        console.log("ROUND 5 START!");
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-}
-
-console.log(`PLAYER'S POINTS : ${humanScore} VS COMPUTER'S POINTS : ${computerScore}`)
-
-if (humanScore > computerScore) {
-    console.log("Congratulations ! You won.");
-} else if(humanScore === computerScore) {
-    console.log("Even ! No Winner yet!");
-} else {
-    console.log("Too bad ! You lose.");
+if (humanScore === 5 && computerScore < 5) {
+    winner.textContent = "YOU";
+    humanScore = 0;
+    computerScore = 0;
+    humanScoreDisplay.textContent = humanScore.toString();
+    computerScoreDisplay.textContent = computerScore.toString();
+} else if (computerScore === 5 && humanScore < 5){
+    winner.textContent = "COMPUTER";
+    humanScore = 0;
+    computerScore = 0;
+    humanScoreDisplay.textContent = humanScore.toString();
+    computerScoreDisplay.textContent = computerScore.toString();
+} else if (humanScore === 0 || computerScore === 0) {
+    winner.textContent = "";
 }
 
 
 };
 
 
-playGame();
+paperButton.addEventListener('click', () => {
+    playRound("Paper", getComputerChoice());
+    humanChoiceDisplay.textContent = "Paper";
+    playGame();
+});
+
+rockButton.addEventListener('click', () => {
+    playRound("Rock", getComputerChoice());
+    humanChoiceDisplay.textContent = "Rock";
+    playGame();
+});
+
+scissorsButton.addEventListener('click', () => {
+    playRound("Scissors", getComputerChoice());
+    humanChoiceDisplay.textContent = "Scissors";
+    playGame();
+    
+});
